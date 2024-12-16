873f0c317215958bb3268270bfc99258d87c4ffc
Muzammil Ahmed <166981810+azurecoders@users.noreply.github.com>
2024-12-16 20:57:35 +0500

    INITIAL COMMIT

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import { FetchAuthUserAction } from "@/action";
 import LogOutComponent from "@/components/logout-component";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
   const { data } = await FetchAuthUserAction();
@@ -9,8 +8,8 @@ export default async function Home() {
   return (
     <div className="flex flex-col gap-3 container mx-auto p-5">
       <h1 className="text-4xl font-bold">NextJS Authentication</h1>
-      <h2 className="text-xl font-semibold">{data.username}</h2>
-      <p className="text-lg">{data.email}</p>
+      <h2 className="text-xl font-semibold">{data?.username}</h2>
+      <p className="text-lg">{data?.email}</p>
       <LogOutComponent />
     </div>
   );
